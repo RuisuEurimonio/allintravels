587634f90a681f27e55475a0ef27d061bbab4ea4
Ruisu Eurimonio <pipelinares2912@gmail.com>
2025-02-13 23:46:55 -0500

    Inicio y configuracion inicial

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+function Header() {
+    return (
+        <header className="static w-full">
+            <img src="" />
+            <ul>
+                <li> Inicio </li>
+                <li> Nosotros </li>
+                <li> Viajes </li>
+                <li> Blogs </li>
+                <li> Contacto </li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
