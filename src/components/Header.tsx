2f6c2b8b3ec6531602734fec906791a26119d199
Ruisu Eurimonio <pipelinares2912@gmail.com>
2025-03-17 01:04:46 -0500

    Se agrega funcion y algunos estilos del menu movile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { FaListUl } from "react-icons/fa";
 function Header() {
+
+    const isOpen : boolean = true;
+
     return (
-        <header className="static w-full flex flex-col items-center gap-1 mt-2">
+        <>
+        <div className="bg-gray-900 p-4 text-white flex justify-between items-center">
+            <h2 className="font-bold">All In Travels.</h2>
+            <FaListUl size={24}/> 
+        </div>
+        <header className={`absolute top-0 left-0 w-full bg-gray-400 h-screen flex flex-col items-center gap-1 mt-2 ${
+          isOpen ? "translate-y-0" : "-translate-y-[100vh]"} 
+          transition-transform duration-300 ease-in-out md:hidden
+        `}>
             <img src="/assets/img/logo.png" className="object-cover max-w-40"/>
             <ul className="flex flex-col text-center">
                 <li className="font-bold"> Inicio. </li>
@@ -11,6 +23,7 @@ function Header() {
                 <li> Contacto </li>
             </ul>
         </header>
+        </>
     )
 }
 
