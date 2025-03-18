1a7237a3a7a3bfa2264f7fb2b79ec52b8c34cbc7
Ruisu Eurimonio <pipelinares2912@gmail.com>
2025-03-18 00:52:09 -0500

    Se ajusta imagen para versión desktop

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ function App() {
     <>
       <header>
         <Header />
-        <div className="text-center font-bold h-[50vh] ">
+        <div className="text-center font-bold h-[50vh] 
+          md:h-[85vh]
+        ">
           <div className="bg-[url('/src/assets/header.png')] h-full bg-cover bg-center bg-no-repeat flex flex-col justify-center">
             <h1 className="text-white">THE TRAVEL</h1>
             <h2 className="text-amber-500 tracking-[1rem]">EXPERIENCE</h2>
