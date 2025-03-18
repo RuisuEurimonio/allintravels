523b177a2418898ac6cccc9c77c3440c62e4ca17
Ruisu Eurimonio <pipelinares2912@gmail.com>
2025-03-18 12:22:38 -0500

    tarjetas agregadas con animación para moviles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import Card from "./components/card"
 import Header from "./components/Header"
 
@@ -5,20 +6,25 @@ function App() {
   
 
   return (
-    <>
+    <div className="w-full overflow-hidden">
       <header>
         <Header />
         <div className="text-center font-bold h-[50vh] 
           md:h-[85vh]
         ">
-          <div className="bg-[url('/src/assets/header.png')] h-full bg-cover bg-center bg-no-repeat flex flex-col justify-center">
+          <div className="bg-[url('/src/assets/header.png')] h-full bg-cover bg-center bg-no-repeat flex flex-col justify-end gap-5 pb-10">
             <h1 className="text-white">THE TRAVEL</h1>
             <h2 className="text-amber-500 tracking-[1rem]">EXPERIENCE</h2>
+            <div className="flex w-[300%] overflow-x-hidden custom-animation">
+              <Card text="Viajes grupales" img="/src/assets/viajes_grupales.png" className="w-[70vw] mx-[15vw]"/>
+              <Card text="Viajes a la medida" img="/src/assets/viajes_a_la_medida.png" className="w-[70vw] mx-[15vw]"/>
+              <Card text="Destinos para parejas" img="/src/assets/destinos_para_parejas.png" className="w-[70vw] mx-[15vw]"/>
+            </div>
           </div>
         </div>
       </header>
-      <Card text="Hola" img="/src/assets/viajes_grupales.png"/>
-    </>
+      <h1> Hola </h1>
+    </div>
   )
 }
 
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,16 +4,17 @@ import { FaArrowCircleRight } from "react-icons/fa";
 interface CardProps {
     text: string;
     img: string;
+    className?: string;
 }
 
-const Card : React.FC<CardProps> = ({text, img}) => {
+const Card : React.FC<CardProps> = ({text, img, className = ""}) => {
 
     return (
-        <div className="border-[.8rem] border-neutral-950/20 rounded-3xl">
-            <div className="relative">
-                <img src={img} className="object-cover"/>
+        <div className={`border-[.8rem] border-neutral-950/50 rounded-3xl ${className}`}>
+            <div className="relative w-full">
+                <img src={img} className="object-cover w-full"/>
                 <div className="absolute bottom-0 text-white right-0 p-1 mr-6">
-                    <a className="flex flex-row items-center gap-2.5"> {text} 
+                    <a className="flex flex-row items-center gap-2.5 text-sm"> {text} 
                         <FaArrowCircleRight />
                     </a>
                 </div>
